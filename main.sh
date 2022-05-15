a8f4bf67aeade6c622a0a468c454afd853b6b0e1
cyzzc <80954721+cyzzc@users.noreply.github.com>
2022-05-15 22:05:05 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,31 @@
+
+#!/bin/bash
+function git_clone() {
+  git clone --depth 1 $1 $2 || true
+ }
+function git_sparse_clone() {
+  branch="$1" rurl="$2" localdir="$3" && shift 3
+  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+  cd $localdir
+  git sparse-checkout init --cone
+  git sparse-checkout set $@
+  mv -n $@ ../
+  cd ..
+  rm -rf $localdir
+  }
+function mvdir() {
+mv -n `find $1/* -maxdepth 0 -type d` ./
+rm -rf $1
+}
+git clone --depth 1 https://github.com/sirpdboy/luci-theme-opentopd
+
+rm -rf ./*/.git & rm -f ./*/.gitattributes
+rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
+
+bash diy/create_acl_for_luci.sh -a >/dev/null 2>&1
+bash diy/convert_translation.sh -a >/dev/null 2>&1
+
+rm -rf create_acl_for_luci.err & rm -rf create_acl_for_luci.ok
+rm -rf create_acl_for_luci.warn
+
+exit 0
