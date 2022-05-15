9d5f1e8041ca8e678739d13ec78693dfe1c4d8b6
cyzzc <80954721+cyzzc@users.noreply.github.com>
2022-05-16 00:57:32 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -21,7 +21,7 @@ git clone --depth 1 https://github.com/sirpdboy/luci-theme-opentopd
 git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff
 git clone --depth 1 https://github.com/destan19/OpenAppFilter && mvdir OpenAppFilter
 git clone --depth 1 https://github.com/Erope/openwrt_nezha && mvdir openwrt_nezha
-git clone --depth 1 https://github.com/sirpdboy/netspeedtest && mvdir netspeedtest
+git clone --depth 1 https://github.com/sirpdboy/netspeedtest && mv -n netspeedtest/luci-app-netspeedtest ./ ; rm -rf netspeedtest
 
 rm -rf ./*/.git & rm -f ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
