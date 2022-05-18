84e72e19e1edcc84e60456d2e6f0cf15e610805d
cyzzc <80954721+cyzzc@users.noreply.github.com>
2022-05-18 20:45:41 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -20,6 +20,7 @@ rm -rf $1
 git clone --depth 1 https://github.com/sirpdboy/luci-theme-opentopd
 git clone --depth 1 https://github.com/esirplayground/luci-app-poweroff
 git clone --depth 1 https://github.com/destan19/OpenAppFilter && mvdir OpenAppFilter
+git clone --depth 1 https://github.com/AlexZhuo/luci-app-bandwidthd.git
 
 rm -rf ./*/.git & rm -f ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
